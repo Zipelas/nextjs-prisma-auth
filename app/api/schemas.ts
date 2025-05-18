660c341ab42a7c3260fd7c4f79059d42bd2c22c2
Andreas Fagerlund <mcshaffa@gmail.com>
2025-05-18 18:08:31 +0200

    put title + content inside {}

--- app/api/schemas.ts
+++ app/api/schemas.ts
@@ -1,7 +1,8 @@
 import { z } from "zod";
 
-export const PostSchema = zod.object({});
-title: z.string().min(4).max(255),
-content: z.string().min(10).max(1000);
+export const PostSchema = z.object({
+  title: z.string().min(4).max(255),
+  content: z.string().min(10).max(1000),
+});
 
 export type PostCreate = z.infer<typeof PostSchema>;
