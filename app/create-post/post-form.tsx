073b1ab401797ccc72eae7a67f75a72b9a951bc6
Andreas Fagerlund <mcshaffa@gmail.com>
2025-05-18 18:09:37 +0200

    wip

--- app/create-post/post-form.tsx
+++ app/create-post/post-form.tsx
@@ -1,28 +1,37 @@
-"use client";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { PostCreate } from "../api/schemas";
-import { savePost } from "../api/actions";
-
-
+'use client';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { savePost } from '../api/actions';
+import { PostCreate, PostSchema } from '../api/schemas';
 
 export default function PostForm() {
-    const form = useForm({});
-    resolver: zodResolver(PostSchema);
+  const form = useForm({
+    resolver: zodResolver(PostSchema),
+  });
 
-    const handleSavePost = (post: PostCreate) => {
-        await savePost(post);
-        // todo: show toast to user...
-    };
+  const handleSavePost = async (post: PostCreate) => {
+    // todo: use RQ mutation for loading state
+    await savePost(post);
+    // todo: show toast to user...
+  };
 
-
-  return ()
-  <form className="grid gap-2 p-2" onSubmit={form.handleSubmit(handleSavePost)}>
-    <h1 className="text-xl">Create new post</h1>
-    <input type="text" {...form.register("title")} placeholder="Titel" className="p-1 rounded border-slate-400"/></input>
-    <textarea {...form.register("content")} placeholder="Innehåll" />
-    <button className="p-1 rounded bg-slate-200">Save</button>
-  </form>
-);
+  return (
+    <form
+      className='grid gap-2 p-2'
+      onSubmit={form.handleSubmit(handleSavePost)}>
+      <h1 className='text-sky-500 text-xl'>Create new Post</h1>
+      <input
+        type='text'
+        {...form.register('title')}
+        placeholder='Title'
+        className='p-1 border rounded border-slate-400'
+      />
+      <textarea
+        {...form.register('content')}
+        placeholder='Content'
+        className='p-1 border rounded border-slate-400'
+      />
+      <button className='bg-sky-500 bg-hover-sky-700 p-1 rounded'>Save</button>
+    </form>
+  );
 }
