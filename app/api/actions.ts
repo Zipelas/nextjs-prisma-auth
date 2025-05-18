1f07ec6bd9a8868e132c399096f0c90070b08bc6
Andreas Fagerlund <mcshaffa@gmail.com>
2025-05-18 18:07:07 +0200

    add comments

--- app/api/actions.ts
+++ app/api/actions.ts
@@ -6,8 +6,10 @@ import { redirect } from 'next/navigation';
 import { PostCreate, PostSchema } from './schemas';
 
 export const savePost = async (data: PostCreate) => {
+  // todo: check if user has access to this endpoint
   const postData = PostSchema.parse(data);
-  const post = await db.post.create({ data: { ...postData, authorId: 1 } });
+  await db.post.create({ data: { ...postData, authorId: 1 } });
+  // todo: get authorId from logged in user...
 
   revalidatePath('/');
   redirect('/');
