90bf990bc87292c5e4c051833b271cec061c29f3
Andreas Fagerlund <mcshaffa@gmail.com>
2025-05-21 18:31:39 +0200

    wip

--- app/api/actions.ts
+++ app/api/actions.ts
@@ -8,7 +8,7 @@ import { PostCreate, PostSchema } from './schemas';
 export const savePost = async (data: PostCreate) => {
   // todo: check if user has access to this endpoint
   const postData = PostSchema.parse(data);
-  await db.post.create({ data: { ...postData, authorId: 1 } });
+  await db.post.create({ data: { ...postData, authorId: "1" } });
   // todo: get authorId from logged in user...
 
   revalidatePath('/');
