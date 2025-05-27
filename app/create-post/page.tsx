22404f19104648ec9302ef931d7747991fc201b3
Andreas Fagerlund <mcshaffa@gmail.com>
2025-05-27 17:32:16 +0200

    changed color on taxt and bg

--- app/create-post/page.tsx
+++ app/create-post/page.tsx
@@ -1,8 +1,8 @@
-import PostForm from './post-form';
+import PostForm from "./post-form";
 
 export default function CreatePost() {
   return (
-    <main className='bg-black'>
+    <main>
       <PostForm />
     </main>
   );
--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,7 +2,7 @@ import GithubButton from './providers/github-button';
 
 export default function SigninPage() {
   return (
-    <main className='bg-black mt-20 grid place-content-center gap-4'>
+    <main className='mt-20 grid place-content-center gap-4'>
       <h1 className='text-4xl text-center'>Login</h1>
       <GithubButton />
     </main>
