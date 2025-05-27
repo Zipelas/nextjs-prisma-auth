eaf0c45fbcd50667e2da6fae9f69132454da742f
Andreas Fagerlund <mcshaffa@gmail.com>
2025-05-27 11:54:05 +0200

    wip

--- app/api/actions.ts
+++ app/api/actions.ts
@@ -2,14 +2,19 @@
 
 import { db } from '@/prisma/client';
 import { revalidatePath } from 'next/cache';
+import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
+import { auth } from '../auth';
 import { PostCreate, PostSchema } from './schemas';
 
 export const savePost = async (data: PostCreate) => {
-  // todo: check if user has access to this endpoint
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session) throw new Error('Not logged in...');
+
   const postData = PostSchema.parse(data);
-  await db.post.create({ data: { ...postData, authorId: "1" } });
-  // todo: get authorId from logged in user...
+  await db.post.create({ data: { ...postData, authorId: session.user.id } });
 
   revalidatePath('/');
   redirect('/');
