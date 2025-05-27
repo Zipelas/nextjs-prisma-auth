0f506c4008401fc6ecd249c0005d19cdfeaec7bc
Andreas Fagerlund <mcshaffa@gmail.com>
2025-05-27 18:06:17 +0200

    changed min to in zod

--- app/api/schemas.ts
+++ app/api/schemas.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const PostSchema = z.object({
-  title: z.string().min(4).max(255),
+  title: z.string().min(3).max(255),
   content: z.string().min(10).max(1000),
 });
 
